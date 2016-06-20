4de1bdcb567e8b437c015f3a3cd339de9985be27
antoniker <jos_mustang@hotmail.com>
2016-06-20 16:12:04 -0500

    Initial Commit

---
+++ Velocimetro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Velocimetro
+//
+//  Created by Ing. José Antonio Franco Cortés on 20/06/16.
+//  Copyright © 2016 xquared. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
